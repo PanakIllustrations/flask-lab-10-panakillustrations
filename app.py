4fe2b6f819de3a257c8ec5219503a20723abe248
Josiah Panak <140780435+PanakIllustrations@users.noreply.github.com>
2024-08-05 05:49:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Josiah Panak in 3308.'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://panakj:RoDM2DMDouqUI5GL3JgWar0JlE2Mu11z@dpg-cqoblgtds78s73bu35s0-a/flask_lab_10_db")
+    conn.close()
+    return "Database Connection Successful."
