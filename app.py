846ea38a6d9ba67559417461e8d2a19128e3a295
Josiah Panak <140780435+PanakIllustrations@users.noreply.github.com>
2024-08-05 07:25:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -28,19 +28,3 @@ def creating():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Created"
-
-@app.route('/db_insert')
-def creating():
-    conn = psycopg2.connect("postgresql://panakj:RoDM2DMDouqUI5GL3JgWar0JlE2Mu11z@dpg-cqoblgtds78s73bu35s0-a/flask_lab_10_db" )
-    cur = conn.cursor()
-    cur.execute('''
-    INSERT INTO Basketball (First, Last, City, Name, Number)
-    Values
-    ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
-    ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
-    ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
-    ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
-    ''')
-    conn.commit()
-    conn.close()
-    return "Basketball Table Successfully Created"
