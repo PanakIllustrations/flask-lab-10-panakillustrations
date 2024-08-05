0decf94d47f159e31995f4f1bbf9dea0422dea24
Josiah Panak <140780435+PanakIllustrations@users.noreply.github.com>
2024-08-05 08:32:53 -0600

    Update app.py function

--- app.py
+++ app.py
@@ -34,12 +34,12 @@ def inserting():
     conn = psycopg2.connect("postgresql://panakj:RoDM2DMDouqUI5GL3JgWar0JlE2Mu11z@dpg-cqoblgtds78s73bu35s0-a/flask_lab_10_db" )
     cur = conn.cursor()
     cur.execute('''
-    INSERT INTO Basketball (First, Last, City, Name, Number)
-Values
-('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
-('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
-('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
-('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
     ''')
     conn.commit()
     conn.close()
@@ -50,19 +50,19 @@ def selecting():
     cconn = psycopg2.connect("postgresql://panakj:RoDM2DMDouqUI5GL3JgWar0JlE2Mu11z@dpg-cqoblgtds78s73bu35s0-a/flask_lab_10_db" )
     cur = conn.cursor()
     cur.execute('''
-    SELECT * FROM Basketball;
-    ''')
-records = cur.fetchall();
-conn.close()
-response_string=""
-response_string+="<table>"
-for player in records:
-    response_string+="<tr>"
-    for info in player:
-        response_string+="<td>{}</td>".format(info)
-    response_string+="</tr>"
-response_string+="</table>"
-return response_string
+        SELECT * FROM Basketball;
+        ''')
+    records = cur.fetchall();
+    conn.close()
+    response_string=""
+    response_string+="<table>"
+    for player in records:
+        response_string+="<tr>"
+        for info in player:
+            response_string+="<td>{}</td>".format(info)
+        response_string+="</tr>"
+    response_string+="</table>"
+    return response_string
 
 @app.route('/db_drop')
 def dropping():
